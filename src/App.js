1b5657f3b515e87ae07424d49217001092b23e18
Sixl-Daniel <sixl.daniel@googlemail.com>
2018-08-20 12:09:21 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import DemoComponent from './components/DemoComponent';
+
+class App extends Component {
+    render() {
+        return (
+            <div className="app">
+                <DemoComponent />
+            </div>
+        );
+    }
+}
+
+export default App;
